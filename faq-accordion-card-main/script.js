63a2b9141b9b0551662b4686587cdce04075b741
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-04-20 15:15:14 -0300

    FAQ accordion card

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,16 @@
+let accordion = document.getElementsByClassName("accordion");
+let i
+
+for(i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle("active");
+    
+    /* Toggle between hiding and showing the active panel */
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    })
+}
